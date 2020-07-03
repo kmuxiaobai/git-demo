3f528a6806196ace9a9c88a6e2083fd39d8aa74b
boge <boge@kmu.com>
2020-07-03 11:36:40 +0800

    第二次提交IndexServlet

--- src/com/kmu/IndexServlet.java
+++ src/com/kmu/IndexServlet.java
@@ -1,7 +1,12 @@
 package com.kmu;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 /**
  * @作者：boge 时间：2020/7/3 11:33
  */
 public class IndexServlet {
+    public static void main(String[] args) {
+        System.out.println("我是IndexServlet类");
+    }
 }
