9afe4abce2d2015948c053888da447618c57c733
boge <boge@kmu.com>
2020-07-03 12:32:48 +0800

    hotfix分支代码

--- src/com/kmu/Hello.java
+++ src/com/kmu/Hello.java
@@ -5,20 +5,6 @@ package com.kmu;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("我是小黑提交了内容");
-        System.out.println("我是小黑提交了内容");
-        System.out.println("我是小黑提交了内容基于e515211这个版本进行了修改");
-        System.out.println("我是小黑提交了内容基于e515211这个版本进行了修改");
-        System.out.println("我是小黑提交了内容基于e515211这个版本进行了修改");
-        System.out.println("我是小黑提交了内容基于e515211这个版本进行了修改");
-        System.out.println("我是小黑提交了内容基于e515211这个版本进行了修改");
-        System.out.println("我是小黑提交了内容基于e515211这个版本进行了修改");
-          System.out.println("我是小白提交了内容");
-          System.out.println("我是小白提交了内容");
-          System.out.println("我是小白提交了内容");
-          System.out.println("我是小白提交了内容");
-          System.out.println("我是小白提交了内容");
-          System.out.println("我是小白提交了内容");
-          System.out.println("我是小白提交了内容");
+        System.out.println("我是hotfix分支中的代码");
     }
 }
