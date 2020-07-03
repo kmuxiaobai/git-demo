46de2811dafc25daef0301f528aa9d116f7b7756
boge <boge@kmu.com>
2020-07-03 11:34:54 +0800

    第一次提交到本地库!!!!

---
+++ src/com/kmu/Hello.java
@@ -0,0 +1,10 @@
+package com.kmu;
+
+/**
+ * @作者：boge 时间：2020/7/3 11:32
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你好，第一次创建，并且使用git");
+    }
+}
---
+++ src/com/kmu/IndexServlet.java
@@ -0,0 +1,7 @@
+package com.kmu;
+
+/**
+ * @作者：boge 时间：2020/7/3 11:33
+ */
+public class IndexServlet {
+}
